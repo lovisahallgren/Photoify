4c21802572ffe78312fdc85b65d7fcc5daa0e0e5
Lovisa Hallgren <lovisahallgren1@gmail.com>
2018-12-18 16:42:41 +0100

    user can now change both password and email

--- app/users/settings.php
+++ app/users/settings.php
@@ -48,6 +48,85 @@ if (isset($_FILES['avatar'])) {
     $statement->bindParam(':id', $id, PDO::PARAM_INT);
 
     $statement->execute();
+
+    //Checks if email is set
+} else if (isset($_POST['current-email'], $_POST['new-email'], $_POST['repeat-new-email'])) {
+    $currentEmail = $_POST['current-email'];
+    $newEmail = $_POST['new-email'];
+    $repeatNewEmail = $_POST['repeat-new-email'];
+    $id = (int) $_SESSION['user']['id'];
+
+    $statement = $pdo->query('SELECT email FROM users WHERE id = :id');
+
+    $statement->bindParam(':id', $id, PDO::PARAM_INT);
+
+    $statement->execute();
+
+    $currentEmailDb = $statement->fetch(PDO::FETCH_ASSOC);
+
+// Checks if typed in current password matches password in database
+    if ($currentEmail == $currentEmailDb['email']) {
+
+// Checks if twice typed in new password matches
+        if ($newEmail == $repeatNewEmail) {
+
+// If yes update password in database
+            $statement = $pdo->prepare('UPDATE users SET email = :email WHERE id = :id');
+// hash password to database
+            $statement->bindParam(':email', $newEmail, PDO::PARAM_STR);
+            $statement->bindParam(':id', $id, PDO::PARAM_INT);
+
+            $statement->execute();
+
+        // If new passwords don't match, die page and echo something
+        } else {
+            die("New emails don't match!");
+        }
+
+        // If old password don't match database password, die page and echo something
+    } else {
+        echo die("Old email doesn't match!");
+    }
+
+    //Checks if password is set
+} else if (isset($_POST['current-password'], $_POST['new-password'], $_POST['repeat-new-password'])) {
+    $currentPassword = $_POST['current-password'];
+    $newPassword = $_POST['new-password'];
+    $repeatNewPassword = $_POST['repeat-new-password'];
+    $id = (int) $_SESSION['user']['id'];
+
+// Get old password from database
+    $statement = $pdo->query('SELECT password FROM users WHERE id = :id');
+
+    $statement->bindParam(':id', $id, PDO::PARAM_INT);
+
+    $statement->execute();
+
+    $currentPasswordDb = $statement->fetch(PDO::FETCH_ASSOC);
+
+// Checks if typed in current password matches password in database
+    if (password_verify($currentPassword, $currentPasswordDb['password'])) {
+
+// Checks if twice typed in new password matches
+        if ($newPassword == $repeatNewPassword) {
+
+// If yes update password in database
+            $statement = $pdo->prepare('UPDATE users SET password = :password WHERE id = :id');
+// hash password to database
+            $statement->bindParam(':password', password_hash($newPassword, PASSWORD_DEFAULT), PDO::PARAM_STR);
+            $statement->bindParam(':id', $id, PDO::PARAM_INT);
+
+            $statement->execute();
+
+        // If new passwords don't match, die page and echo something
+        } else {
+            die("New passwords don't match!");
+        }
+
+        // If old password don't match database password, die page and echo something
+    } else {
+        echo die("Old password doesn't match!");
+    }
 }
 
 redirect('/../profile.php');
--- profile.php
+++ profile.php
@@ -2,12 +2,13 @@
 
 require __DIR__.'/views/header.php';
 ?>
+<section class="profile-page">
 
 <form class="form-group" action="/app/users/settings.php" method="post" enctype="multipart/form-data">
     <label for="avatar">Choose an avatar image to upload</label>
     <input type="file" accept=".jpeg" name="avatar" required>
-    <button type="submit" name="button">Upload</button>
-</form>
+    <button class="confirm-btn" type="submit" name="button">Upload</button>
+</form><!-- /form-group -->
 
 <?php if (isset($_SESSION['user'])): ?>
     <img src="<?= '/app/users/avatar/'.$_SESSION['user']['avatar']?>" alt="">
@@ -15,25 +16,38 @@ require __DIR__.'/views/header.php';
 
 <form class="form-group" action="/app/users/settings.php" method="post">
     <label for="biography">Write something in your biography</label>
-    <textarea name="biography"></textarea>
-    <button type="submit" name="button">Submit</button>
-</form>
-
-
+    <textarea class="bio-field" name="biography"></textarea>
+    <button class="confirm-btn" type="submit" name="button">Submit</button>
+</form><!-- /form-group -->
 
 <?php if (isset($_SESSION['user'])): ?>
-    <p><?php
-    $query = $pdo->query('SELECT * FROM users');
-    while($user = $query->fetch(PDO::FETCH_ASSOC)){
-        echo $user['biography'];
-    }
-    ?>
+    <p> <?php
+        $query = $pdo->query('SELECT * FROM users');
+        while($user = $query->fetch(PDO::FETCH_ASSOC)) {
+            echo $user['biography'];
+        } ?>
     </p>
 <?php endif; ?>
 
+<form class="form-group" action="/app/users/settings.php" method="post" enctype="multipart/form-data">
+    <label for="email">Change email</label>
+    <input class="form-control" type="email" name="current-email" placeholder="current email">
+    <input class="form-control" type="email" name="new-email" placeholder="new email">
+    <input class="form-control" type="email" name="repeat-new-email" placeholder="repeat new email">
+    <!-- <small class="form-text text-muted">Change email address.</small> -->
+    <button class="confirm-btn" type="submit" name="button">Confirm email</button>
+</form><!-- /form-group -->
 
+<form class="form-group" action="/app/users/settings.php" method="post" enctype="multipart/form-data">
+    <label for="password">Change password</label>
+    <input class="form-control" type="password" name="current-password" placeholder="current password">
+    <input class="form-control" type="password" name="new-password" placeholder="new password">
+    <input class="form-control" type="password" name="repeat-new-password" placeholder="repeat new password">
+    <!-- <small class="form-text text-muted">Change password.</small> -->
+    <button class="confirm-btn" type="submit" name="button">Confirm password</button>
+</form><!-- /form-group -->
 
-
+</section>
 
 <?php
 
