876ce6073b200c367f9271985e89a055c52a5142
Lovisa Hallgren <lovisahallgren1@gmail.com>
2018-12-19 13:12:18 +0100

    fixed bug on in profile.php

--- app/users/settings.php
+++ app/users/settings.php
@@ -4,40 +4,7 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
-// Checks if there is an image to upload
-if (isset($_FILES['avatar'])) {
-    $avatar = $_FILES['avatar'];
-    $extension = pathinfo($_FILES['avatar']['name'])['extension'];
-    $userName = $_SESSION['user']['user_name'];
-    $filePath = __DIR__.'/avatar/';
-    $id = (int) $_SESSION['user']['id'];
-
-    $avatarName = $userName.'.'.$extension;
-
-// Checks if image is of right kind and isn't to big
-    if ($avatar['type']!== 'image/jpeg') {
-        echo 'The image file type is not allowed.';
-    } elseif ($avatar['size'] >= 3000000) {
-        echo 'The uploaded file exceeded the filesize limit.';
-    } else {
-
-// Updates the database column avatar with the set image
-        $statement = $pdo->prepare('UPDATE users SET avatar = :avatar WHERE id = :id');
-
-        $statement->bindParam(':avatar', $avatarName, PDO::PARAM_STR);
-        $statement->bindParam(':id', $id, PDO::PARAM_INT);
-
-        $statement->execute();
-
-// Moves image to directory
-        move_uploaded_file($avatar['tmp_name'], $filePath.$avatarName);
-    }
-
-// Updates session variable to use the newly set image as avatar
-    $_SESSION['user']['avatar'] = $avatarName;
-
-//Checks if biography is set
-} elseif (isset($_POST['biography'])) {
+if (isset($_POST['biography'])) {
     $biography = $_POST['biography'];
     $id = (int) $_SESSION['user']['id'];
 
--- profile.php
+++ profile.php
@@ -4,7 +4,7 @@ require __DIR__.'/views/header.php';
 ?>
 <section class="profile-page">
 
-    <form class="form-group" action="/app/users/settings.php" method="post" enctype="multipart/form-data">
+    <form class="form-group" action="/app/users/avatar.php" method="post" enctype="multipart/form-data">
         <label for="avatar">Choose an avatar image to upload</label>
         <input type="file" accept=".jpeg" name="avatar" required>
         <button class="confirm-btn" type="submit" name="button">Upload</button>
@@ -12,7 +12,9 @@ require __DIR__.'/views/header.php';
     <?php if (isset($_SESSION['user'])): ?>
         <img src="<?= '/app/users/avatar/'.$_SESSION['user']['avatar']?>" alt="">
     <?php endif; ?>
+    </form><!-- /form-group -->
 
+    <form class="form-group" action="/app/users/settings.php" method="post" enctype="multipart/form-data">
         <label for="biography">Write something in your biography</label>
         <textarea class="bio-field" name="biography"></textarea>
 
@@ -29,9 +31,9 @@ require __DIR__.'/views/header.php';
         <input class="form-control" type="password" name="current-password" placeholder="current password">
         <input class="form-control" type="password" name="new-password" placeholder="new password">
         <input class="form-control" type="password" name="repeat-new-password" placeholder="repeat new password">
+        <button class="update-btn" type="submit" name="button">Update</button>
     </form><!-- /form-group -->
 
-    <button class="update-btn" type="submit" name="button">Update</button>
 </section> <!-- /profile-page -->
 
 <?php
