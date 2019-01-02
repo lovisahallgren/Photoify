0f3cb1403fff99b6b49e4c09caca5aa9b12d4362
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-01-02 17:56:21 +0100

    styled profile page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,9 +11,55 @@ hamburgerIcon.addEventListener('click', () => {
 
 // Images on profile
 const smallPosts = document.querySelectorAll('.small-post');
+const description = document.querySelectorAll('.description');
+const editButtons = document.querySelectorAll('.edit-btn');
+const hearts = document.querySelectorAll('.not-liked');
 
 smallPosts.forEach(smallPost => {
     smallPost.addEventListener('click', () => {
         smallPost.classList.add('big-post');
+        const exits = document.querySelector('.exit');
+        exits.classList.remove('hidden');
+        exits.addEventListener('click', () => {
+            smallPost.classList.remove('big-post');
+            exits.classList.add('hidden');
+        });
+    });
+});
+
+hearts.forEach(heart => {
+    heart.addEventListener('click', () => {
+        heart.classList.add('hidden');
+        const liked = document.querySelectorAll('.liked');
+        liked.forEach(like => {
+            like.classList.remove('hidden');
+            like.addEventListener('click', () => {
+                like.classList.add('hidden');
+                heart.classList.remove('hidden');
+            });
+        });
+    });
+});
+
+editButtons.forEach(editButton => {
+    editButton.addEventListener('click', () => {
+        const editForms = document.querySelectorAll('.edit-form');
+        editForms.forEach(editForm => {
+            editForm.classList.add('edit-form-visible');
+            const hideBtns = document.querySelectorAll('.post-buttons');
+            hideBtns.forEach(hideBtn => {
+                hideBtn.classList.add('hidden');
+            });
+            const cancelBtns = document.querySelectorAll('.cancel-btn');
+            cancelBtns.forEach(cancelBtn => {
+                cancelBtn.addEventListener('click', () => {
+                    editForm.classList.remove('edit-form-visible');
+                    const hideBtns = document.querySelectorAll('.post-buttons');
+                    hideBtns.forEach(hideBtn => {
+                        hideBtn.classList.remove('hidden');
+                    });
+                });
+            });
+        });
     });
 });
--- profile.php
+++ profile.php
@@ -23,7 +23,6 @@ require __DIR__.'/views/header.php';
         <?php if (isset($_SESSION['user'])): ?>
             <img src="<?= '/app/users/avatar/'.$_SESSION['user']['avatar']?>" alt="">
         <?php endif; ?>
-
     </div>
 
     <div class="edit-profile">
@@ -43,11 +42,61 @@ require __DIR__.'/views/header.php';
         } ?>
     </div>
 
+    <div class="posts-header">
+        <div class="">
+            <a class="logo" href="./index.php">
+                <img src="logo.png" alt="Logo">
+            </a>
+        </div>
+        <div class="username">
+            <?php if (isset($_SESSION['user'])) {
+                echo $_SESSION['user']['user_name'];
+            } ?>
+        </div>
+    </div>
+
     <article class="posts">
         <?php $posts = getPostsByUser($_SESSION['user']['id'], $pdo);?>
         <?php foreach ($posts as $post) : ?>
+        <i class="fas fa-times exit hidden"></i>
         <div class="small-post">
             <img src="<?= 'app/posts/uploads/'.$_SESSION['user']['id'].'/'.$post['image']?>" alt="">
+            <div class="like">
+                <p class="post-date"><?php if ($post['updated_at']) {
+                    echo $post['updated_at'];
+                } else {
+                    echo $post['created_at'];
+                }?></p>
+                <i class="far fa-heart not-liked visible"></i>
+                <!-- <div class="liked-div"> -->
+                    <i class="fas fa-heart liked hidden"></i>
+                <!-- </div> -->
+            </div>
+            <div class="description">
+                <p class="username-post"><?= $_SESSION['user']['user_name']; ?></p>
+                <p><?= $post['description']?></p>
+            </div>
+
+            <form class="edit-form" action="app/posts/update.php" method="post">
+                <div class="edit-inputs">
+                    <label for="update-description">Edit your description</label>
+                    <textarea class="update-description-field" name="update-description"
+                    placeholder="<?= $post['description'] ?>" required></textarea>
+                </div>
+                <div class="edit-buttons">
+                    <button class="btn-primary" type="submit" name="post-id" value="<?= $post['id']; ?>">Update text</button>
+                    <button class="cancel-btn" type="button" name="cancel-btn">Cancel</button>
+                </div>
+            </form>
+
+            <div class="post-buttons">
+                <!-- <a href="/app/posts/update.php"> -->
+                    <button class="edit-btn" type="button" name="button">Edit post</button>
+                <!-- </a> -->
+                <!-- <a href="/app/posts/delete.php"> -->
+                    <button class="delete-btn"  type="button" name="button">Delete post</button>
+                <!-- </a> -->
+            </div>
         </div>
     <?php endforeach; ?>
     </article>
