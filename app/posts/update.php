5c74562b57419d0de0c9aafe7d3f1ef5e4c662f9
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-01-10 00:08:22 +0100

    added session message

--- app/posts/update.php
+++ app/posts/update.php
@@ -9,27 +9,23 @@ if (isset($_POST['update-description'], $_POST['post-id'])) {
     $updateDescription = trim(filter_var($_POST['update-description'], FILTER_SANITIZE_STRING));
     $userId = (int) $_SESSION['user']['id'];
 
-        // if (filter_var($updateDescription, FILTER_SANITIZE_STRING)) {
+    $dateAndTime = date('Y-m-d H:i:s');
 
-            $dateAndTime = date('d F Y');
+    $statement = $pdo->prepare('UPDATE posts SET description = :description, updated_at = :updated_at WHERE id = :id');
 
-            $statement = $pdo->prepare('UPDATE posts SET description = :description, updated_at = :updated_at WHERE id = :id');
+    if (!$statement) {
+        die(var_dump($pdo->errorInfo()));
+    }
 
-            if (!$statement) {
-                die(var_dump($pdo->errorInfo()));
-            }
+    $statement->bindParam(':description', $updateDescription, PDO::PARAM_STR);
+    $statement->bindParam(':updated_at', $dateAndTime, PDO::PARAM_STR);
+    $statement->bindParam(':id', $postId, PDO::PARAM_INT);
 
-            $statement->bindParam(':description', $updateDescription, PDO::PARAM_STR);
-            $statement->bindParam(':updated_at', $dateAndTime, PDO::PARAM_STR);
-            $statement->bindParam(':id', $postId, PDO::PARAM_INT);
+    $statement->execute();
 
-            $statement->execute();
-
-            // $desc = $statement->fetch(PDO::FETCH_ASSOC);
-
-            $_SESSION['message'] = 'Your changes has been updated';
-            redirect('/profile.php');
-        }
+    $_SESSION['message'] = 'Your changes has been updated';
+    redirect('/profile.php');
+}
         // else {
         //     $_SESSION['message'] = 'Your changes didn\'t update. Please try again.';
         //
