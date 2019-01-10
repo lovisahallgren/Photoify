e45f3e22ccd75082ae504fa3b15f1241751b8b8e
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-01-10 15:25:46 +0100

    made functions for counting likes and checking if post is like by user

--- app/functions.php
+++ app/functions.php
@@ -35,11 +35,34 @@ function getAllPosts($pdo) {
 
         $statement = $pdo->prepare('SELECT posts.id, posts.image, users.id as user_id, users.username, posts.description, posts.created_at, posts.updated_at FROM posts JOIN users ON posts.user_id = users.id ORDER BY created_at DESC');
 
-        // $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
-
         $statement->execute();
 
         $allPosts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
         return $allPosts;
+}
+
+function countLikes($postId, $pdo) {
+    $statement = $pdo->prepare('SELECT COUNT(*) FROM likes WHERE post_id = :post_id');
+
+    $statement->bindParam(':post_id', $postId, PDO::PARAM_INT);
+
+    $statement->execute();
+
+    $likes = $statement->fetch(PDO::FETCH_ASSOC);
+
+    return $likes["COUNT(*)"];
+}
+
+function isLikedByUser($postId, $userId, $pdo) {
+    $statement = $pdo->prepare('SELECT * FROM likes WHERE post_id = :post_id AND user_id = :user_id');
+
+    $statement->bindParam(':user_id', $userId, PDO::PARAM_INT);
+    $statement->bindParam(':post_id', $postId, PDO::PARAM_INT);
+
+    $statement->execute();
+
+    $isLikedByUser = $statement->fetch(PDO::FETCH_ASSOC);
+
+    return $isLikedByUser ? true : false;
 }
